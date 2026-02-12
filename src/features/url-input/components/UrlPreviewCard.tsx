22b4c0ab1602b2d3566fdbfec0c1b79ec6646ef9
Sherry90 <hhlsdkm@gmail.com>
2026-02-12 14:18:36 +0900

    refactor: UrlPreviewSection 컴포넌트 분해
    컴포넌트 책임 분리 및 재사용성 향상
    - UrlPreviewCard: 썸네일 + 정보 표시
    - UrlPreviewRangeControl: 구간 선택 UI
    - useUrlDownload: 다운로드 로직 훅
    - UrlPreviewSection: 87줄 감소 (조합 컴포넌트로 간소화)

---
+++ src/features/url-input/components/UrlPreviewCard.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { formatDuration } from '@/features/timeline/utils/timeFormatter';
+
+interface UrlPreviewCardProps {
+  title: string;
+  thumbnail?: string;
+  duration: number;
+  maxDuration: number;
+}
+
+/**
+ * URL 미리보기 카드 (썸네일 + 제목 + 시간)
+ */
+export function UrlPreviewCard({
+  title,
+  thumbnail,
+  duration,
+  maxDuration,
+}: UrlPreviewCardProps) {
+  return (
+    <>
+      {/* Thumbnail */}
+      {thumbnail && (
+        <div className="relative w-full aspect-video bg-black">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={thumbnail}
+            alt={title}
+            className="w-full h-full object-contain"
+          />
+        </div>
+      )}
+
+      {/* Info */}
+      <div className="p-5 pb-0">
+        <h3 className="text-[15px] font-medium text-[#d9dce3] mb-1 line-clamp-2">
+          {title}
+        </h3>
+        <p className="text-[12px] text-[#74808c] mb-5">
+          {formatDuration(duration)}
+          {duration > maxDuration && (
+            <span className="ml-2 text-amber-400">
+              (max {maxDuration / 60}min per download)
+            </span>
+          )}
+        </p>
+      </div>
+    </>
+  );
+}
